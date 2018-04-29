c5aeea6635327a075cbd7249aee82b18b61f3406
KimDongjae <dong4810@hotmail.com>
2018-04-30 02:21:08 +0900

    config 정리

--- config/default.js
+++ config/default.js
@@ -13,36 +13,6 @@ module.exports = {
       },
       json: true,
     },
-    saveMeta: async function saveMeta(channelId, channelTitle, channelDesc, channelUrl, channelThumbnails, channelPublishedAt){
-      var client = require('../lib/connection.js')
-
-      await client.index({
-        index: 'channels',
-        type: '_doc',
-        id: channelId,
-        body: {
-          "name": channelTitle,
-          "detail": channelDesc,
-          "url": channelUrl,
-          "thumbnails": channelThumbnails,
-          "publishedAt": channelPublishedAt,
-          "isActive": true
-        }
-      }, function(err, resp, status){
-        console.log(resp)
-      });
-    },
-    saveData: async function saveData(file, channelId, channelTitle, channelUrl, channelThumbnails, channelViewCount, channelSubscriberCount, channelVideoCount, channelPublishedAt, channelCrawledAt){
-      await file.write('{\nchannel_id: ' + channelId + ',\n')
-      await file.write('channel_name: ' + channelTitle + ',\n')
-      await file.write('channel_url: ' + channelUrl + ',\n')  // get url
-      await file.write('channel_thumbnails: ' + channelThumbnails + ',\n')  // can choose default(88), medium(240), high(800)
-      await file.write('channel_viewCount: ' + channelViewCount + ',\n')
-      await file.write('channel_subscriberCount: ' + channelSubscriberCount + ',\n')
-      await file.write('channel_videoCount: ' + channelVideoCount + ',\n')
-      await file.write('channel_publishedAt: ' + channelPublishedAt + ',\n')
-      await file.write('channel_crawledAt: ' + channelCrawledAt + '\n}')
-    },
   },
   videoList: {
     retryOpt: {
@@ -73,19 +43,6 @@ module.exports = {
         part: 'id, snippet, statistics',
       },
       json: true,
-    },
-    saveData: async function saveData(file, videoId, videoTitle, videoUrl, videoThumbnails, videoCategory, videoViewCount, videoLikeCount, videoDislikeCount, videoCommentCount, videoPublishedAt, videoCrawledAt){
-      await file.write('{\nvideo_id: ' + videoId + ',\n')
-      await file.write('video_name: ' + videoTitle + ',\n')
-      await file.write('video_url: ' + videoUrl + ',\n')  // get url
-      await file.write('video_thumbnails: ' + videoThumbnails + ',\n')  // can choose default(88), medium(240), high(800)
-      await file.write('video_category: ' + videoCategory + ',\n')
-      await file.write('video_viewCount: ' + videoViewCount + ',\n')
-      await file.write('video_likeCount: ' + videoLikeCount + ',\n')
-      await file.write('video_dislikeCount: ' + videoDislikeCount + ',\n')
-      await file.write('video_commentCount: ' + videoCommentCount + ',\n')
-      await file.write('video_publishedAt: ' + videoPublishedAt + ',\n')
-      await file.write('video_crawledAt: ' + videoCrawledAt + '\n}')
     },
   },
 }
