e65a041f4fb382070170ea3c38a664512b11a904
KimDongjae <dong4810@hotmail.com>
2018-04-30 02:10:29 +0900

    index에서 개발 환경에 대해서만 특정 코드 수행

--- index.js
+++ index.js
@@ -19,49 +19,55 @@ let channelRaw = fs.readFileSync('./filtered.csv', 'utf8').split(/\r?\n/),
 
 for(let channel of channelRaw) {
   channelList.push(channel.substring(31))
-  if(channelList.length >= 10) // option(have to erase on product)
-    break
 }
 
-channelDetail.setWriter(async (result, channel, time) => {
-  const fileName = `${config.rawDir}/channel-detail/${result.id}-${time}-channel-detail.txt`
-  const file = fs.createWriteStream(fileName)
+// development env wirter setting
+if(!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  for(let channel of channelRaw) {
+    channelList.push(channel.substring(31))
+    if(channelList.length >= 10) // option(have to erase on product)
+      break
+  }
 
-  file.write(JSON.stringify(result, null, '\t'))
-  file.end()
-})
+  channelDetail.setWriter(async (result, channel, time) => {
+    const fileName = `${config.rawDir}/channel-detail/${result.id}-${time}-channel-detail.txt`
+    const file = fs.createWriteStream(fileName)
 
-videoList.setWriter(async (result) => {
-  await mkdirp(result.dirName)
-})
+    file.write(JSON.stringify(result, null, '\t'))
+    file.end()
+    console.log('Channel Detail ' + channel + ' done')
+  })
 
-videoDetail.setWriter(async (result, dirName, videoId, time) => {
-  const fileName = `${dirName}/${videoId}-${time}-video-detail.txt`
-  const file = fs.createWriteStream(fileName)
+  videoList.setWriter(async (result, channel) => {
+    await mkdirp(result.dirName)
+    console.log('Video List ' + channel + ' done')
+  })
 
-  file.write(JSON.stringify(result, null, '\t'))
-  file.end()
-})
+  videoDetail.setWriter(async (result, dirName, videoId, time) => {
+    const fileName = `${dirName}/${videoId}-${time}-video-detail.txt`
+    const file = fs.createWriteStream(fileName)
+
+    file.write(JSON.stringify(result, null, '\t'))
+    file.end()
+  })
+}
 
 /* fetch jobs on main */
 const main = async (channelList) => {
   /* run queue & save data */
-
   for(const channel of channelList){
     try {
       console.log('this is ' + channel)
-      const time = (new Date()).toFormat('YYYY-MM-DDTHH24-MI-SS.000Z')
 
+      const time = (new Date()).toFormat('YYYY-MM-DDTHH24-MI-SS.000Z')
       let videoCount = await channelDetail.collect(channel, time)
 
+      // check running channel
       if(videoCount === '0' || videoCount === 0) continue
-      else console.log('Channel Detail ' + channel + ' done')
-
       let { dirName, videos } = await videoList.collect(channel, videoCount)
-      console.log('Video List ' + channel + ' done')
 
       for(const videoId of videos){
-        let r = videoDetail.collect(dirName, videoId)
+        await videoDetail.collect(dirName, videoId)
       }
     } catch (error){
       console.log(`error on channel: ${channel}\nmaybe invalid channel?\nerror: ${error}`)
